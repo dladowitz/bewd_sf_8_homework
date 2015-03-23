a2fee23b3a2246ffc199e23093334f9305d92d76
David Ladowitz <david@ladowitz.com>
2015-03-23 14:30:46 -0700

    fixing typos

--- MikelBlake-mikelblake/midterm/lib/reddit.rb
+++ MikelBlake-mikelblake/midterm/lib/reddit.rb
@@ -12,16 +12,17 @@ class Reddit
 		reddit = JSON.load(RestClient.get("http://www.reddit.com/search.json?q=#{user_search_term}"))
 		story_array = reddit['data']['children']
 
-      story_array.each do |story|
-        title = story['data']['title']
-        selftext = story['data']['selftext']
-        url = story['data']['url']
-        @stories << Story.new(title, selftext, url)
-      end 
-   end
+    story_array.each do |story|
+      title = story['data']['title']
+      selftext = story['data']['selftext']
+      url = story['data']['url']
+      @stories << Story.new(title, selftext, url)
+    end 
+  end
 
 	def print_stories
 		@stories.each do |story|
 			puts @stories
-	end
+	  end
+  end
 end
--- MikelBlake-mikelblake/midterm/lib/story.rb
+++ MikelBlake-mikelblake/midterm/lib/story.rb
@@ -1,6 +1,6 @@
 class Story
 
-  attr_accessor :stories :title, :selftext, :url
+  attr_accessor :stories, :title, :selftext, :url
     def initialize(title, selftext, url)
       @stories = []
       @title = title
